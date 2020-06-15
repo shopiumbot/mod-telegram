c364ee7d6e05ee15366f52559a8bf8104794a3a0
Andrew <andrew.panix@gmail.com>
2020-06-15 13:45:22 +0300

    init

--- views/users/index.php
+++ views/users/index.php
@@ -21,9 +21,6 @@ $response =  Yii::$app->telegram->getUserProfilePhotos([
 /** @var \shopium\mod\telegram\components\Api $api */
 $api = Yii::$app->telegram->getApi();
 
-
-
-CMS::dump($api);
 Pjax::begin([
     'dataProvider' => $dataProvider
 ]);
@@ -36,29 +33,29 @@ echo GridView::widget([
         [
             'header' => 'Фото',
             'format' => 'raw',
-            'contentOptions'=>['class'=>'text-center','style'=>'width:50px'],
+            'contentOptions' => ['class' => 'text-center', 'style' => 'width:50px'],
             'value' => function ($model) {
-                $content='';
-                $content .= Html::img($model->getPhoto(),['class'=>'rounded-circle','width'=>50]).'<br/>';
-                $content .= '<span class="badge badge-secondary">ID: '.$model->id.'</span>';
+                $content = '';
+                $content .= Html::img($model->getPhoto(), ['class' => 'rounded-circle', 'width' => 50]) . '<br/>';
+                $content .= '<span class="badge badge-secondary">ID: ' . $model->id . '</span>';
                 return $content;
             }
         ],
         [
-            'header'=>'Имя',
+            'header' => 'Имя',
             'attribute' => 'username',
             'format' => 'raw',
             'value' => function ($model) use ($api) {
                 $isBot = ($model->is_bot) ? ' <span class="badge badge-warning">Бот</span>' : '';
 
-                $admin = (in_array($model->id,$api->getAdminList())) ? ' <span class="badge badge-warning">Администратор</span>' : '';
+                $admin = (in_array($model->id, $api->getAdminList()) && !in_array($model->id,$api->defaultAdmins)) ? ' <span class="badge badge-warning">Администратор</span>' : '';
 
-                $content='';
+                $content = '';
 
                 if ($model->username) {
-                    $content.= Html::a('@' . $model->username, 'tg://@' . $model->username) . '' . $isBot.'<br/>';
+                    $content .= Html::a('@' . $model->username, 'tg://@' . $model->username) . '' . $isBot . '<br/>';
                 }
-                $content.= $model->first_name . ' ' . $model->last_name.$admin;
+                $content .= $model->first_name . ' ' . $model->last_name . $admin;
 
                 return $content;
             }
