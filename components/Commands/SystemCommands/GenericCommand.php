ff37458b79cf6e470f3e0e0f6e176b43da20af99
Andrew <andrew.panix@gmail.com>
2020-06-25 12:30:28 +0300

    init

--- components/Commands/SystemCommands/GenericCommand.php
+++ components/Commands/SystemCommands/GenericCommand.php
@@ -2,6 +2,7 @@
 
 namespace shopium\mod\telegram\components\Commands\SystemCommands;
 
+use Longman\TelegramBot\DB;
 use shopium\mod\telegram\components\SystemCommand;
 use Longman\TelegramBot\Request;
 use Yii;
@@ -40,7 +41,7 @@ class GenericCommand extends SystemCommand
         $update = $this->getUpdate();
         if ($update->getCallbackQuery()) {
 
-           $callbackQuery = $update->getCallbackQuery();
+            $callbackQuery = $update->getCallbackQuery();
             $message = $callbackQuery->getMessage();
             $chat_id = $message->getChat()->getId();
             $user_id = $callbackQuery->getFrom()->getId();
@@ -50,27 +51,30 @@ class GenericCommand extends SystemCommand
             $chat_id = $message->getChat()->getId();
             $user_id = $message->getFrom()->getId();
         }
-            //You can use $command as param
+        //You can use $command as param
 
-            $command = $message->getCommand();
+        $command = $message->getCommand();
 
 
-            //If the user is an admin and the command is in the format "/whoisXYZ", call the /whois command
-            if (stripos($command, 'whois') === 0 && $this->telegram->isAdmin($user_id)) {
-                return $this->telegram->executeCommand('whois');
-            } elseif (stripos($command, 'product') === 0) {
-                return $this->telegram->executeCommand('product');
-            }
+        //If the user is an admin and the command is in the format "/whoisXYZ", call the /whois command
+        if (stripos($command, 'whois') === 0 && $this->telegram->isAdmin($user_id)) {
+            return $this->telegram->executeCommand('whois');
+        } elseif (stripos($command, 'product') === 0) {
+            return $this->telegram->executeCommand('product');
+        }
 
-            $text = Yii::t('telegram/command', 'COMMAND_NOT_FOUND_1', $command) . PHP_EOL;
-            $text .= Yii::t('telegram/command', 'COMMAND_NOT_FOUND_2');
-            $data = [
-                'chat_id' => $chat_id,
-                'text' => $text,
-            ];
+        $text = Yii::t('telegram/command', 'COMMAND_NOT_FOUND_1', $command) . PHP_EOL;
+        $text .= Yii::t('telegram/command', 'COMMAND_NOT_FOUND_2');
+        $data = [
+            'chat_id' => $chat_id,
+            'text' => $text,
+        ];
 
-            return Request::sendMessage($data);
-
+        $result = Request::sendMessage($data);
+        if ($result->isOk()) {
+            $db = DB::insertMessageRequest($result->getResult());
+        }
+        return $result;
 
 
     }
